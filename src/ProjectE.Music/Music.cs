59a20ff5efca58c497d819c9acef1237d2166d02
Simon <frasselk@gmail.com>
2024-04-01 01:24:21 +0200

    Database setup with two enpoints Create/GetById

---
+++ src/ProjectE.Music/Music.cs
@@ -0,0 +1,54 @@
+﻿using Ardalis.GuardClauses;
+
+namespace ProjectE.Music;
+
+internal class Music
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public string SongName { get; private set; } = string.Empty;
+    public string Artist { get; private set; } = string.Empty;
+    public string Genre { get; private set; } = string.Empty; //Should be enumerable list of genres
+
+    internal Music(Guid id, string songName, string artist, string genre) 
+    { 
+        Id = Guard.Against.Default(id);
+        SongName = Guard.Against.NullOrEmpty(songName);
+        Artist = Guard.Against.NullOrEmpty(artist);
+        Genre = Guard.Against.NullOrEmpty(genre);
+    }
+
+    // TODO: update Name/Artist?
+    //internal void UpdateArtist(string newSongName)
+    //{
+    //    SongName = Guard.Against.NullOrEmpty(newSongName);
+    //}
+    //...
+}
+
+internal enum SongGenre
+{
+    Rock,
+    Pop,
+    HipHop,
+    Jazz,
+    Blues,
+    Country,
+    Electronic,
+    RnB,
+    Classical,
+    Reggae,
+    Metal,
+    Folk,
+    Punk,
+    Rap,
+    Indie,
+    Dance,
+    Soul,
+    Funk,
+    Gospel,
+    World,
+    Latin,
+    Alternative,
+    Experimental,
+    Ambient
+}
---
+++ src/ProjectE.Music/MusicService.cs
@@ -0,0 +1,47 @@
+﻿using ProjectE.Music.Data;
+
+namespace ProjectE.Music;
+
+internal class MusicService : IMusicService
+{
+    private readonly IMusicRepository _musicRepository;
+    public MusicService(IMusicRepository musicRepository)
+    {
+        _musicRepository = musicRepository;
+    }
+
+    public async Task CreateMusicAsync(MusicDto newMusic)
+    {
+        var music = new Music(newMusic.Id, newMusic.SongName, newMusic.Artist, newMusic.Genre);
+
+        await _musicRepository.AddAsync(music);
+        await _musicRepository.SaveChangesAsync();
+    }
+
+    public async Task DeleteMusicAsync(Guid id)
+    {
+        var deleteMusic = await _musicRepository.GetByIdAsync(id);
+        if (deleteMusic is not null) 
+        { 
+            await _musicRepository.DeleteAsync(deleteMusic);
+            await _musicRepository.SaveChangesAsync();
+        }
+    }
+
+    public async Task<MusicDto> GetMusicByIdAsync(Guid id)
+    {
+        var music = await _musicRepository.GetByIdAsync(id);
+
+        // TODO: if not found?
+        return new MusicDto(music!.Id, music.SongName, music.Artist, music.Genre);
+    }
+
+    public async Task<List<MusicDto>> ListMusicAsync()
+    {
+        var music = (await _musicRepository.ListAsync())
+            .Select(music => new MusicDto(music.Id, music.SongName, music.Artist, music.Genre))
+            .ToList();
+
+        return music;
+    }
+}
