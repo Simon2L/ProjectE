aab91e18bbebad3ad7c419e20088ff3965d3c0c2
Simon <frasselk@gmail.com>
2024-04-25 13:31:40 +0200

    added Music related to emojis endpoint

---
+++ src/ProjectE.Music/MusicEnpoints/ListMusicRelatedToEmojis.cs
@@ -0,0 +1,33 @@
+﻿using FastEndpoints;
+
+namespace ProjectE.Music.MusicEnpoints;
+
+internal class ListMusicRelatedToEmojis(IMusicService musicService) : 
+    Endpoint<ListMusicRelatedToEmojisRequest, ListMusicRelatedToEmojisResponse>
+{
+    private readonly IMusicService _musicService = musicService;
+
+    public override void Configure()
+    {
+        Get("/music/emojis");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(ListMusicRelatedToEmojisRequest req, 
+        CancellationToken ct)
+    {
+        var music = await _musicService.ListMusicRelatedToEmojisAsync(req);
+
+        await SendAsync(new ListMusicRelatedToEmojisResponse()
+        {
+            Music = music
+        });
+    }
+}
+
+public class ListMusicRelatedToEmojisResponse
+{
+    public List<MusicDto> Music { get; set; } = [];
+}
+
+public record ListMusicRelatedToEmojisRequest(string FirstEmoji, string SecondEmoji, string ThirdEmoji);
