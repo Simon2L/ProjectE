339587d61d0b4a152ba5560d3be00d494a5cf2f9
m0chi/ <aly0mal99@gmail.com>
2024-04-29 13:47:32 +0100

    quiz functionality + testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Quiz from './components/Quiz/Quiz'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Quiz />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
