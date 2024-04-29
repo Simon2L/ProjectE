7d643303af4e8932c1fdbc1689e8c9404ae66dee
m0chi/ <aly0mal99@gmail.com>
2024-04-29 13:45:49 +0100

    more template components

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p className="footer_info">me and simon</p>
+    </footer>
+    /* here supposed to be two <a></a> tags with names & links to github */
+  )
+}
+
+export default Footer
---
+++ client/src/components/Results/Results.tsx
@@ -0,0 +1,15 @@
+const Results = () => {
+  return (
+   <ul className="results">
+      <li className="results__wrapper">
+        <p className="results__item"></p>
+      </li>
+   </ul> 
+   /* 5 results */
+   /* resultList.map((item) => (
+        <html here>
+   )) */
+  )
+}
+
+export default Results;
