4bd0b7ead596f5274c1f83c6c27ac32872f2c95b
Simon <frasselk@gmail.com>
2024-05-02 12:57:57 +0200

    added movie favorites

--- src/ProjectE.Users/ApplicationUser.cs
+++ src/ProjectE.Users/ApplicationUser.cs
@@ -7,11 +7,14 @@ internal class ApplicationUser : IdentityUser
 {
     private readonly List<Song> _favoriteSongs = [];
     public IReadOnlyCollection<Song> FavoriteSongs => _favoriteSongs.AsReadOnly();
+    private readonly List<Movie> _favoriteMovies = [];
+    public IReadOnlyCollection<Movie> FavoriteMovies =>  _favoriteMovies.AsReadOnly();
 
     public void AddSongToFavorites(Song song)
     {
         Guard.Against.Null(song);
 
+        // TODO: ADD IF SONG ALREADY EXISTS IN FAVORITES
         //var existingMusic = _favorites.FirstOrDefault(m => m.Id == music.Id);
 
         //if (existingMusic is not null)
@@ -21,5 +24,11 @@ internal class ApplicationUser : IdentityUser
 
         _favoriteSongs.Add(song);
 
+    }
+
+    public void AddMovieToFavorites(Movie movie) 
+    { 
+        Guard.Against.Null(movie);
+        _favoriteMovies.Add(movie);
     }
 }
---
+++ src/ProjectE.Users/MovieConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ProjectE.Users;
+
+internal class MovieConfiguration : IEntityTypeConfiguration<Movie>
+{
+    public void Configure(EntityTypeBuilder<Movie> builder)
+    {
+        builder.Property(item => item.Id)
+            .ValueGeneratedNever();
+
+        builder.Property(item => item.Emoji)
+            .IsUnicode();
+    }
+}
