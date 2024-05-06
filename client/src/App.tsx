86c034f7fb5aa99b77ee419602633f6cf0dc6bfe
m0chi/ <aly0mal99@gmail.com>
2024-05-06 14:41:54 +0100

    removed MUI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,20 +3,16 @@ import Signup from './components/Signup/Signup'
 import Login from './components/Login/Login'
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
-import {pageTheme} from './themes/page.css'
-import ThemeProvider from '@mui/material/styles/ThemeProvider'
 
 function App() {
-  const theme = pageTheme;
 
   return (
-    
-    <ThemeProvider theme={theme}>
+    <>
       <Header />
       <Main />
       <Signup />
       <Login />
-    </ThemeProvider>
+    </>
   )
 }
 
