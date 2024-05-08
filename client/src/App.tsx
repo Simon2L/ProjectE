d95252eba625296cedb4732b86ad8b9949e3c8d1
Simon <frasselk@gmail.com>
2024-05-08 19:31:19 +0200

    added react router dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,24 @@
 import './App.css'
-// import Signup from './components/Signup/Signup'
-// import Login from './components/Login/Login'
+import Signup from './components/Signup/Signup'
+import Login from './components/Login/Login'
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
 import Footer from './components/Footer/Footer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Main />
-      {/* <Signup />
-      <Login /> */}
+      <Routes>
+        <Route path='/' element={<Main/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<Signup/>} />
+        <Route path='*' element={<h1 className='text-8xl text-center'>Not Found 😭</h1>}></Route>
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
