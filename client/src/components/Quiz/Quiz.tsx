2600089c3160edae897d0df664016f220416cdec
m0chi/ <aly0mal99@gmail.com>
2024-05-08 15:21:05 +0100

    filled star svg

--- client/src/components/Quiz/Quiz.tsx
+++ client/src/components/Quiz/Quiz.tsx
@@ -86,7 +86,7 @@ const Quiz = (props : IProps) => {
             <li className="flex justify-between items-center min-w-[600px] px-4 py-2 gap-4 w-60 h-16 rounded-md bg-transparent bg-opacity-70 text-2xl uppercase border-[3px] border-black" key={song.id}>
               {song.artist} {song.songName} {song.emoji}
               <button className="hover:scale-110 ease-in duration-150 hue-rotate-90 hover:hue-rotate-0" onClick={() => handleAddToFavorites(song)}>
-                <img className="max-w-[24px] max-h-[24px]"src={star}/>
+                <img className="max-w-[24px] max-h-[24px]"src={star} />
               </button>
             </li>
           ))}
