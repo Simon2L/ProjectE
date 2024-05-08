0b8f5bae361408f0090c7423b743d424c965d5f9
Simon <frasselk@gmail.com>
2024-05-08 20:56:19 +0200

    removed hue style

--- client/src/components/Results/MoviesResults.tsx
+++ client/src/components/Results/MoviesResults.tsx
@@ -1,13 +1,16 @@
 import { IMovie, IMovieResult } from "../../Interfaces/interfaces"
 import star from "../../assets/star.svg"
+import filledStar from "../../assets/star-filled.svg"
+import { useState } from "react"
 
 interface IProps {
     movieResults: IMovieResult | undefined
 }
 const MoviesResults = (props: IProps) => {
-
+    const [isFavorite, setIsFavorite] = useState(true);
     const handleAddToFavorites = (movie: IMovie) => {
         console.log(movie)
+        setIsFavorite(true)
         //addMovieToFavorites(movie)
     }
 
@@ -18,8 +21,8 @@ const MoviesResults = (props: IProps) => {
                     <li className="flex justify-between items-center min-w-[600px] px-4 py-2 gap-4 w-60 h-16 rounded-md bg-transparent
                     bg-opacity-70 text-2xl uppercase border-[3px] border-black" key={movie.id}>
                         {movie.id} {movie.title} {movie.rating} {movie.emoji}
-                        <button className="hover:scale-110 ease-in duration-150 hue-rotate-90 hover:hue-rotate-0" onClick={() => handleAddToFavorites(movie)}>
-                            <img className="max-w-[24px] max-h-[24px]" src={star} />
+                        <button className="hover:scale-110 ease-in duration-150 hover:hue-rotate-0" onClick={() => handleAddToFavorites(movie)}>
+                            <img className="max-w-[24px] max-h-[24px]" src={filledStar} />
                         </button>
                     </li>
                 ))}
