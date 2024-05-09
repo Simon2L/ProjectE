9432908f4913ed25122a4e8ff0d750cedfeb7830
Simon <frasselk@gmail.com>
2024-05-09 22:36:17 +0200

    gane service

---
+++ src/ProjectE.Games/GameService.cs
@@ -0,0 +1,20 @@
+﻿using ProjectE.Games.Data;
+
+namespace ProjectE.Games;
+
+internal class GameService(IGameRepository gameRepository) : IGameService
+{
+    private readonly IGameRepository _gameRepository = gameRepository;
+
+    public async Task<List<GameDto>> ListGamesRelatedToEmojis(ListGamesRelatedToEmojisRequest req)
+    {
+        var games = (await _gameRepository.ListAsync()).Where(game => req.FirstEmoji.Contains(game.Emoji)
+             || req.SecondEmoji.Contains(game.Emoji)
+             || req.ThirdEmoji.Contains(game.Emoji))
+            .Take(5)
+            .Select(game => new GameDto(game.Id, game.Name, game.Rating, game.Emoji))
+            .ToList();
+
+        return games;
+    }
+}
