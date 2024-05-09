8904ac2c55c933544f55a2537e503104fdfef791
Simon <frasselk@gmail.com>
2024-05-09 21:30:20 +0200

    Add movie to favorites

---
+++ src/ProjectE.Users/UseCases/Favorites/Movie/AddMovieToFavoritesCommandValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+
+namespace ProjectE.Users.UseCases.Favorites.Movie;
+
+public class AddMovieToFavoritesCommandValidator : AbstractValidator<AddMovieToFavoritesCommand>
+{
+    public AddMovieToFavoritesCommandValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .WithMessage("EmailAddress is required");
+
+        RuleFor(x => x.Movie.Id)
+            .NotEmpty()
+            .WithMessage("Not a valid SongId");
+
+        RuleFor(x => x.Movie.Title)
+            .NotEmpty()
+            .WithMessage("Song name cannot be empty");
+
+        RuleFor(x => x.Movie.Emoji)
+            .NotEmpty()
+            .WithMessage("Artist cannot be empty");
+
+        RuleFor(x => x.Movie.Rating)
+            .NotEmpty()
+            .WithMessage("Ratinmg cannot be empty");
+    }
+}
