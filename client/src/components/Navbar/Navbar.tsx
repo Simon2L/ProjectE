a9a063f6a074248eb4884e601f637a814e6dd0a5
Simon <frasselk@gmail.com>
2024-05-09 17:10:44 +0200

    temp welcome the user

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -16,6 +16,7 @@ const Navbar = () => {
             <>
             <Link className="px-2 py-1 shadow-[0_7px_0_rgb(0,0,0)] hover:shadow-[0_3px_0px_rgb(0,0,0)] text-black bg-white bg-opacity-20 ease-out hover:translate-y-1 transition-all rounded" to="/">Home</Link>
             <Link className="px-2 py-1 shadow-[0_7px_0_rgb(0,0,0)] hover:shadow-[0_3px_0px_rgb(0,0,0)] text-black bg-white bg-opacity-20 ease-out hover:translate-y-1 transition-all rounded" to="/favorites">Favorites</Link>
+            <h2>Welcome {user.username}!</h2>
             </>
           )}
         </div>
