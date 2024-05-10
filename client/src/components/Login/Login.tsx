5672f4aa9c61dd816bb34fa45d988d99c6ad5073
Simon <frasselk@gmail.com>
2024-05-10 13:55:59 +0200

    put in  middle

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -35,18 +35,20 @@ const Login = (props: IProps) => {
     }
 
     return (
-        <form className="flex flex-col max-w-sm justify-center items-center gap-4" onSubmit={handleSubmit}>
-            <h2 className="text-4xl">Login now!</h2>
+        <div className="grid place-items-center">
+            <form className="flex flex-col max-w-sm justify-center items-center gap-4" onSubmit={handleSubmit}>
+                <h2 className="text-4xl">Login now!</h2>
 
-            <label className="text-xl mr-2">Email</label>
-            <input className="p-2 text-xl rounded-md min-h-11" onChange={e => setEmail(e.target.value)} type="email" />
+                <label className="text-xl mr-2">Email</label>
+                <input className="p-2 text-xl rounded-md min-h-11" onChange={e => setEmail(e.target.value)} type="email" />
 
-            <label className="text-xl mr-2">Password</label>
-            <input className="p-2 text-xl rounded-md min-h-11" onChange={e => setPassword(e.target.value)} type="password" />
+                <label className="text-xl mr-2">Password</label>
+                <input className="p-2 text-xl rounded-md min-h-11" onChange={e => setPassword(e.target.value)} type="password" />
 
-            <button disabled={disableButton} className="max-w-24 min-w-24 min-h-8 rounded-md bg-indigo-600 text-white hover:bg-cyan-600">Login!</button>
-            {unathorized && <span className=" text-red-600 p-2 bg-white rounded-lg">Incorrect email or password</span>}
-        </form>
+                <button disabled={disableButton} className="max-w-24 min-w-24 min-h-8 rounded-md bg-indigo-600 text-white hover:bg-cyan-600">Login!</button>
+                {unathorized && <span className=" text-red-600 p-2 bg-white rounded-lg">Incorrect email or password</span>}
+            </form>
+        </div>
     )
 }
 
--- client/src/components/Signup/Signup.tsx
+++ client/src/components/Signup/Signup.tsx
@@ -6,27 +6,29 @@ const Signup = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
-    const handleSubmit = async (event : FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
-        const response = await signup({username: username, email: email, password: password})
+        const response = await signup({ username: username, email: email, password: password })
         console.log(response)
     }
 
     return (
-        <form className="flex flex-col max-w-sm justify-center items-center gap-4" onSubmit={handleSubmit}>
-            <h2 className="text-4xl">Signup now!</h2>
+        <div className="grid place-items-center">
+            <form className="flex flex-col max-w-sm justify-center items-center gap-4" onSubmit={handleSubmit}>
+                <h2 className="text-4xl">Signup now!</h2>
 
-            <label className="text-xl mr-2">Username</label>
-            <input className="p-2 text-xl rounded-md min-h-11" onChange={e => setUsername(e.target.value)} type="text"/>
+                <label className="text-xl mr-2">Username</label>
+                <input className="p-2 text-xl rounded-md min-h-11" onChange={e => setUsername(e.target.value)} type="text" />
 
-            <label className="text-xl mr-2">Email</label>
-            <input className="p-2 text-xl rounded-md min-h-11" onChange={e => setEmail(e.target.value)} type="email"/>
+                <label className="text-xl mr-2">Email</label>
+                <input className="p-2 text-xl rounded-md min-h-11" onChange={e => setEmail(e.target.value)} type="email" />
 
-            <label className="text-xl mr-2">Password</label>
-            <input className="p-2 text-xl rounded-md min-h-11" onChange={e => setPassword(e.target.value)} type="password"/>
+                <label className="text-xl mr-2">Password</label>
+                <input className="p-2 text-xl rounded-md min-h-11" onChange={e => setPassword(e.target.value)} type="password" />
 
-            <button className="max-w-24 min-w-24 min-h-8 rounded-md bg-indigo-600 text-white hover:bg-cyan-600">Signup!</button>
-        </form>
+                <button className="max-w-24 min-w-24 min-h-8 rounded-md bg-indigo-600 text-white hover:bg-cyan-600">Signup!</button>
+            </form>
+        </div>
     )
 }
 
