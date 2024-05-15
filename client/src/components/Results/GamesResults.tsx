dc4624d474e199ecdd9e2082e5ae41e108f0731c
m0chi/ <aly0mal99@gmail.com>
2024-05-15 16:18:25 +0100

    game + movie results size

--- client/src/components/Results/GamesResults.tsx
+++ client/src/components/Results/GamesResults.tsx
@@ -13,7 +13,7 @@ const GamesResults = (props: IProps) => {
         <div>
             <ul className="flex flex-col list-none gap-6 max-h-[366px]">
                 {props.gameResults?.games.map((game: IGame) => (
-                    <li className="flex justify-between items-center min-w-[600px] px-4 py-2 gap-4 w-60 h-16 rounded-md text-2xl uppercase bg-white bg-opacity-20 shadow-[0_4px_0_rgb(0,0,0)]" key={game.id}>
+                    <li className="flex justify-between items-center min-w-[500px] px-4 py-4 gap-4 w-60 h-12 rounded-md text-xl uppercase bg-white bg-opacity-20 shadow-[0_4px_0_rgb(0,0,0)]" key={game.id}>
                         {game.emoji} {game.name} {game.publishYear}
                         <StarButton handleAddToFavorites={handleAddToFavorites} add={game} />
                     </li>
--- client/src/components/Results/MoviesResults.tsx
+++ client/src/components/Results/MoviesResults.tsx
@@ -12,7 +12,7 @@ const MoviesResults = (props: IProps) => {
         <div>
             <ul className="flex flex-col list-none gap-6 max-h-[366px]">
                 {props.movieResults?.movies.map((movie: IMovie) => (
-                    <li className="flex justify-between items-center min-w-[600px] px-4 py-2 gap-4 w-60 h-16 rounded-md text-2xl uppercase bg-white bg-opacity-20 shadow-[0_4px_0_rgb(0,0,0)]" key={movie.id}>
+                    <li className="flex justify-between items-center min-w-[500px] px-4 py-4 gap-4 w-60 h-12 rounded-md text-xl uppercase bg-white bg-opacity-20 shadow-[0_4px_0_rgb(0,0,0)]" key={movie.id}>
                         {movie.emoji} {movie.title} {movie.rating}
                         <StarButton handleAddToFavorites={handleAddToFavorites} add={movie} />
                     </li>
