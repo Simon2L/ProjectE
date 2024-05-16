84e15e3179cba92c0a073f8c2ce9e80b36018770
Simon <frasselk@gmail.com>
2024-05-16 16:06:09 +0200

    remove from favorites

--- client/src/components/Favorites/Favorites.tsx
+++ client/src/components/Favorites/Favorites.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { IFavorites, getAllFavorites } from "../../utils/favoritesService"
 import { IGame, IMovie, ISong } from "../../Interfaces/interfaces"
 import QuizResultsSkeleton from "../Skeletons/QuizResultSkeleton"
+import { removeSongFromFavorites } from "../../utils/musicService"
 
 const Favorites = () => {
     const [favorites, setFavorites] = useState<IFavorites>()
@@ -9,6 +10,7 @@ const Favorites = () => {
         const response = getAllFavorites()
         response.then((fav) => setFavorites(fav))
     }, [])
+
 
     return (
         <div className=" flex flex-col items-center gap-10">
@@ -19,9 +21,9 @@ const Favorites = () => {
                     {favorites?.music.map((song: ISong) => (
                         <li className="flex justify-between items-center min-w-[600px] px-4 py-2 gap-4 w-60 h-16 rounded-md text-2xl uppercase  bg-white bg-opacity-20 shadow-[0_4px_0_rgb(0,0,0)]" key={song.id}>
                             {song.artist} {song.songName} {song.emoji}
-                            {/* <button className="hover:scale-110 ease-in duration-150 hover:hue-rotate-0" onClick={() => handleAddToFavorites(song)}>
-                <img className="max-w-[24px] max-h-[24px]" src={star} />
-              </button> */}
+                            <button className="hover:scale-110 ease-in duration-150 hover:hue-rotate-0" onClick={() => removeSongFromFavorites(song.id)}>
+                                Remove
+                            </button>
                         </li>
                     ))}
                 </ul>
