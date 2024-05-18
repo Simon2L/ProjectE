f93344880410327ab9f540872ec8bc2e85a6db09
m0chi/ <aly0mal99@gmail.com>
2024-05-18 17:54:04 +0100

    footer

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,12 +1,14 @@
+import github from "../../assets/github.png"
+
 const Footer = () => {
   return (
-    <footer className="flex flex-col items-end font-nunito">
+    <footer className="font-nunito uppercase font-semibold">
       <ul>
         <li>
-          <a href="https://github.com/Simon2L">Simon</a>
+          <a href="https://github.com/Simon2L"><img className="w-4" src={github}/>Simon</a>
         </li>
         <li>
-          <a href="https://github.com/m0chimoon">M0chi</a>
+          <a href="https://github.com/m0chimoon"><img className="w-4" src={github} />M0chi</a>
         </li>
       </ul>
     </footer>
