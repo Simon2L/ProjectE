8fafec572a96b0e06c850dbdaadb1402884f23f0
Simon <frasselk@gmail.com>
2024-05-21 23:01:34 +0200

    bg white

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -38,10 +38,10 @@ const Login = (props: IProps) => {
             <form className="flex flex-col max-w-sm justify-center items-center gap-4 font-nunito font-bold uppercase" onSubmit={handleSubmit}>
 
                 <label className="text-xl mr-2">Email 📧</label>
-                <input className="p-2 text-xl rounded-md min-h-11" onChange={e => setEmail(e.target.value)} type="email" />
+                <input className="bg-white p-2 text-xl rounded-md min-h-11" onChange={e => setEmail(e.target.value)} type="email" />
 
                 <label className="text-xl mr-2">Password 🔑</label>
-                <input className="p-2 text-xl rounded-md min-h-11" onChange={e => setPassword(e.target.value)} type="password" />
+                <input className="bg-white p-2 text-xl rounded-md min-h-11" onChange={e => setPassword(e.target.value)} type="password" />
 
                 <button disabled={disableButton} className="mt-24 uppercase text-lg px-3 py-1 shadow-[0_4px_0_rgb(0,0,0)] hover:shadow-[0_3px_0px_rgb(0,0,0)] text-black bg-white bg-opacity-20 ease-out hover:translate-y-1 transition-all rounded">Login</button>
                 {unathorized && <span className=" text-red-600 p-2 bg-white rounded-lg">Incorrect email or password</span>}
--- client/src/components/Signup/Signup.tsx
+++ client/src/components/Signup/Signup.tsx
@@ -23,13 +23,13 @@ const Signup = () => {
           <form className="flex flex-col max-w-sm justify-center items-center gap-4" onSubmit={handleSubmit}>
 
               <label className="text-xl mr-2">Username 🧑‍🍳</label>
-              <input className="p-2 text-xl rounded-md min-h-11" onChange={e => setUsername(e.target.value)} type="text" />
+              <input className="bg-white p-2 text-xl rounded-md min-h-11" onChange={e => setUsername(e.target.value)} type="text" />
 
               <label className="text-xl mr-2">Email 📧</label>
-              <input className="p-2 text-xl rounded-md min-h-11" onChange={e => setEmail(e.target.value)} type="email" />
+              <input className="bg-white p-2 text-xl rounded-md min-h-11" onChange={e => setEmail(e.target.value)} type="email" />
 
               <label className="text-xl mr-2">Password 🔑</label>
-              <input className="p-2 text-xl rounded-md min-h-11" onChange={e => setPassword(e.target.value)} type="password" />
+              <input className="bg-white p-2 text-xl rounded-md min-h-11" onChange={e => setPassword(e.target.value)} type="password" />
 
               <button className="mt-24 uppercase text-lg px-3 py-1 shadow-[0_4px_0_rgb(0,0,0)] hover:shadow-[0_3px_0px_rgb(0,0,0)] text-black bg-white bg-opacity-20 ease-out hover:translate-y-1 transition-all rounded">Signup</button>
           </form>
