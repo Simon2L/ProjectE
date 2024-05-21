5139ee1e115257908f5f1fe4e94bd2daaaee4235
Simon <frasselk@gmail.com>
2024-05-21 23:01:49 +0200

    responsive removal of favorites

--- client/src/components/Favorites/Favorites.tsx
+++ client/src/components/Favorites/Favorites.tsx
@@ -23,7 +23,18 @@ const Favorites = () => {
                     {favorites?.music.map((song: ISong) => (
                         <li className="flex justify-between items-center min-w-[600px] px-4 py-2 gap-4 w-60 h-16 rounded-md text-2xl uppercase  bg-white bg-opacity-20 shadow-[0_4px_0_rgb(0,0,0)]" key={song.id}>
                             {song.artist} {song.songName} {song.emoji}
-                            <button className="hover:scale-110 ease-in duration-150" onClick={() => removeSongFromFavorites(song.id)}>
+                            <button className="hover:scale-110 ease-in duration-150" onClick={() => {
+                                removeSongFromFavorites(song.id);
+                                setFavorites((prev) => {
+                                    if (!prev) return prev
+                                    return {
+                                        ...prev,
+                                        music: prev.music.filter((s) => s.id !== song.id),
+                                        movies: prev.movies ? prev.movies : [],
+                                        games: prev.games ? prev.games : []
+                                    };
+                                });
+                                }}>
                                 Remove
                             </button>
                         </li>
@@ -37,7 +48,18 @@ const Favorites = () => {
                     {favorites?.movies?.map((movie: IMovie) => (
                         <li className="flex justify-between items-center min-w-[600px] px-4 py-2 gap-4 w-60 h-16 rounded-md text-2xl uppercase  bg-white bg-opacity-20 shadow-[0_4px_0_rgb(0,0,0)]" key={movie.id}>
                             {movie.title} {movie.rating.toFixed(1)} {movie.emoji}
-                            <button className="hover:scale-110 ease-in duration-150 hover:hue-rotate-0" onClick={() => removeMovieFromFavorites(movie.id)}>
+                            <button className="hover:scale-110 ease-in duration-150 hover:hue-rotate-0" onClick={() => {
+                                removeMovieFromFavorites(movie.id)
+                                setFavorites((prev) => {
+                                    if (!prev) return prev
+                                    return {
+                                        ...prev,
+                                        music: prev.music ? prev.music : [],
+                                        movies: prev.movies.filter((m) => m.id !== movie.id),
+                                        games: prev.games ? prev.games : []
+                                    };
+                                });
+                                }}>
                                 Remove
                             </button>
                         </li>
@@ -51,7 +73,18 @@ const Favorites = () => {
                     {favorites?.games?.map((game: IGame) => (
                         <li className="flex justify-between items-center min-w-[600px] px-4 py-2 gap-4 w-60 h-16 rounded-md text-2xl uppercase bg-white bg-opacity-20 shadow-[0_4px_0_rgb(0,0,0)]" key={game.id}>
                             {game.name} {game.rating} {game.emoji}
-                            <button className="hover:scale-110 ease-in duration-150 hover:hue-rotate-0" onClick={() => removeGameFromFavorites(game.id)}>
+                            <button className="hover:scale-110 ease-in duration-150 hover:hue-rotate-0" onClick={() => {
+                                removeGameFromFavorites(game.id)
+                                setFavorites((prev) => {
+                                    if (!prev) return prev
+                                    return {
+                                        ...prev,
+                                        music: prev.music ? prev.music : [],
+                                        movies: prev.movies ? prev.movies : [],
+                                        games: prev.games.filter((g) => g.id !== game.id),
+                                    };
+                                });
+                                }}>
                                 Remove
                             </button>
                         </li>
